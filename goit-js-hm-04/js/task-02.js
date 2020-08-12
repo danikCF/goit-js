6a0ae326d72c3c1c23ec64b1d7f1336953a24e3a
danikCF <danik20002902@gmail.com>
2020-08-12 15:38:55 +0300

    Module-4 js Done

--- goit-js-hm-04/js/task-02.js
+++ goit-js-hm-04/js/task-02.js
@@ -1,9 +1,31 @@
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+console.log('homework#4(task2)');
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
 
-console.log(matrix[0][0]); // 1
-console.log(matrix[1]+[2]); // 6
-console.log(matrix[2][2]); // 9
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
