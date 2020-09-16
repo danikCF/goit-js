4923a407038615e4dbc2ac078dce38ac4bc9984e
danikCF <danik20002902@gmail.com>
2020-09-16 13:39:19 +0300

    Module-2 js Done

--- goit-js-hm-02/js/task-3.js
+++ goit-js-hm-02/js/task-3.js
@@ -17,4 +17,4 @@ const findLongestWord = function(string) {
   
   console.log(findLongestWord('Google do a roll')); // 'Google'
   
-  console.log(findLongestWord('May the force be with you')); // 'forcee'
+  console.log(findLongestWord('May the force be with you')); // 'force'
